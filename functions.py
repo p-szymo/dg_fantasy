e2fa09f9adcc765cd96ebacbe15f81ffb6ad9efb
“p-szymo” <joshua.szymanowski@gmail.com>
2023-10-20 23:29:39 -0400

    tied Event EventSearch and Search together

--- functions.py
+++ functions.py
@@ -2,42 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import itertools
-import datetime.date as dt
-
-
-def soupify(url):
-    page = requests.get(url)
-    soup = bs(page.content, "html.parser")
-    return soup
-
-
-def event_parser_initial(url):
-
-    # _base_url = 'https://www.pdga.com/'
-
-    soup = soupify(url)
-    table = soup.select('div[class*="table-container"]')[0]
-
-    odd_rows = table.select('tr[class*="odd"]')
-    even_rows = table.select('tr[class*="even"]')
-
-    rows = [x for x in itertools.chain.from_iterable(itertools.zip_longest(odd_rows,even_rows)) if x]
-    
-    parsings = []
-    for row in rows:
-        if row.select('td[class*="views-field views-field-Classification"]')[0].text.strip() == 'Pro':
-            event_link = row.select('a[href*="/tour/event/"]')[0]
-            event_url = _base_url + event_link['href']
-            event_official_name = event_link.text
-            event_number = int(event_url.split('/')[-1])
-            parsings.append((event_url, event_official_name, event_number))
-            
-    if len(parsings) > 1:
-        print('There may be an issue as we parsed more than one item that matches.')
-
-    event = parsings[0]
-
-    return event[0], event[1], event[2]
+from datetime import date as dt
 
 
 def events_list(
@@ -58,37 +23,6 @@ def events_list(
     events_with_links = [(e.text,'https://www.pdga.com/'+e['href']) for e in events_with_links_raw if e.text] #   and '<img' not in e
 
     return events_with_links
-
-
-def parse_rows(row):
-    if row.select('td[class*="par"]'):
-        score = row.select('td[class*="par"]')[0].text
-    elif row.select('td[class*="dnf"]'):
-        score = "DNF"
-    else:
-        score = "ERROR"
-    result = { # could also do round scores, round ratings, and total score
-        'Place': int(row.select_one('td[class*="place"]').text),
-        'Player': row.select_one('td[class*="player"]').text,
-        'PDGA Number': int(row.select_one('td[class*="pdga-number"]').text),
-        'Player Rating': int(row.select_one('td[class*="player-rating"]').text),
-        'Score': score,
-    }
-    return result
-
-
-def event_parser_results(event):
-    
-    soup = soupify(event.event_url)
-    soup_table = soup.select('div[class*="leaderboard"]')[0]
-    results_table_raw = soup_table.select('div[class*="table-container"]')[0]
-    odd_rows = results_table_raw.select('tr[class*="odd"]')
-    even_rows = results_table_raw.select('tr[class*="even"]')
-    results_raw = [x for x in itertools.chain.from_iterable(itertools.zip_longest(odd_rows,even_rows)) if x]
-    results_list = [parse_rows(row) for row in results_raw]
-    results_df = pd.DataFrame(data=results_list) #.set_index('Place')
-        
-    return results_df
 
 
 def total_scorer(results, players, verbose=0):
@@ -180,7 +114,9 @@ def type_check(item, _type):
 
 class Search:
 
-    def __init__(self, search_type, search_term, year=dt.today().year, **kwargs):
+    def __init__(self, search_type, **kwargs):
+
+        self._search_type = search_type.title()
 
         self._search_options = {
             'Event': {
@@ -212,8 +148,8 @@ class Search:
             'last_name': {'url': 'LastName', 'type': str}
         }
 
-        self._base_url = self.search_options[search_type.title()][base_url]
-        self._search_reqs = self.search_options[search_type.title()][reqs]
+        self._base_url = self._search_options[self._search_type]['base_url']
+        self._search_reqs = self._search_options[self._search_type]['reqs']
 
         _reqs_not_met = [x for x in self._search_reqs if x not in kwargs.keys()]
 
@@ -231,12 +167,14 @@ class Search:
 
         self.search_string = self._base_url + '?'
 
-        for item in self._search_reqs:
+        _number_of_reqs = len(self._search_reqs)
+
+        for i, item in enumerate(self._search_reqs):
             item_url_term = self._search_dict[item]['url']
             item_req_type = self._search_dict[item]['type']
             item_input = self.input[item]
 
-            type_check(item_url, item_req_type)
+            type_check(item_input, item_req_type)
 
             if item_req_type == list:
                 url_part = '&'.join([f'{item_url_term}={x}' for x in item_input])
@@ -247,103 +185,152 @@ class Search:
             else:
                 print('Something went wrong with _search_reqs')
 
-            self.search_string += url_part
+            self.search_string += url_part.replace(' ','%20')
+
+            if i != _number_of_reqs - 1:
+                self.search_string += '&'
+
+        self._soup = self.soupify(self.search_string)
 
 
-        self.result_soup = soupify(self.search_string)
+    def soupify(self, url):
+        page = requests.get(url)
+        soup = bs(page.content, "html.parser")
+        return soup
 
 
 
 
 
 
-class EventSearch:
+class EventSearch(Search):
 
-    def __init__(self, search_name, year, tier=['ES', 'M'], classification=['Pro']):
+    # def __init__(self, search_name, year, tier=['ES', 'M'], classification=['Pro']):
 
-        self.search_name = search_name.strip()
-        self.year = int(year)
+    #     self.search_name = search_name.strip()
+    #     self.year = int(year)
         
-        self._base_url = 'https://www.pdga.com/tour/search?'
-        self._event_name = self.search_name.replace(' ','%20')
-        self._min_date = f'{self.year}-01-01'
-        self._max_date = f'{self.year}-12-31'
-        self._tier = tier
-        self._classification = classification
+    #     self._base_url = 'https://www.pdga.com/tour/search?'
+    #     self._event_name = self.search_name.replace(' ','%20')
+    #     self._min_date = f'{self.year}-01-01'
+    #     self._max_date = f'{self.year}-12-31'
+    #     self._tier = tier
+    #     self._classification = classification
 
-        _tier_search = '&'.join([f'Tier[]={t}' for t in self._tier])
-        _classification_search = '&'.join([f'Classification[]={c}' for c in self._classification])
+    #     _tier_search = '&'.join([f'Tier[]={t}' for t in self._tier])
+    #     _classification_search = '&'.join([f'Classification[]={c}' for c in self._classification])
 
-        self.search_url = self._base_url \
-                            + 'OfficialName=' + self._event_name \
-                            + '&date_filter[min][date]=' + self._min_date \
-                            + '&date_filter[max][date]=' + self._max_date \
-                            + '&' + _tier_search \
-                            + '&' + _classification_search
+    #     self.search_url = self._base_url \
+    #                         + 'OfficialName=' + self._event_name \
+    #                         + '&date_filter[min][date]=' + self._min_date \
+    #                         + '&date_filter[max][date]=' + self._max_date \
+    #                         + '&' + _tier_search \
+    #                         + '&' + _classification_search
 
-        print(f'Searching here: {self.search_url}')
+    #     print(f'Searching here: {self.search_url}')
 
 
-    def search(self):
+    def __init__(self, search_type, **kwargs):
+        Search.__init__(self, search_type, **kwargs)
+
+        self._event_details = self.parser()
         
-        soup = soupify(self.search_url)
+        self.event_url = self._event_details[0]
+        self.event_official_name = self._event_details[1]
+        self.event_number = self._event_details[2]
 
-        event_details = event_parser_initial(self.search_url)
+
+    def parser(self):
+
+        _base_url = 'https://www.pdga.com'
+
+        soup = self._soup
+        table = soup.select('div[class*="table-container"]')[0]
+
+        odd_rows = table.select('tr[class*="odd"]')
+        even_rows = table.select('tr[class*="even"]')
+
+        rows = [x for x in itertools.chain.from_iterable(itertools.zip_longest(odd_rows,even_rows)) if x]
         
-        self.event_url = event_details[0]
-        self.event_official_name = event_details[1]
-        self.event_number = event_details[2]
+        parsings = []
+        for row in rows:
+            if row.select('td[class*="views-field views-field-Classification"]')[0].text.strip() == 'Pro':
+                event_link = row.select('a[href*="/tour/event/"]')[0]
+                event_url = _base_url + event_link['href']
+                event_official_name = event_link.text
+                event_number = int(event_url.split('/')[-1])
+                parsings.append((event_url, event_official_name, event_number))
+                
+        if len(parsings) > 1:
+            print('There may be an issue as we parsed more than one item that matches.')
 
-        return event_details
+        event = parsings[0]
+
+        return event[0], event[1], event[2]
 
 
-    def getEventName(self):
-        return self.event_official_name
-
-
-    def getEventNumber(self):
-        return self.event_number
-
-
-class Event:
+class Event(EventSearch):
 
     def __init__(self, name, year, tier=['ES', 'M'], classification=['Pro']):
-        # super(Event, self).__init__(**kwargs)
+        
         self.name = name.strip()
         self.year = int(year)
         
-        self._base_url = 'https://www.pdga.com/tour/search?'
         self._event_name = self.name.replace(' ','%20')
         self._min_date = f'{self.year}-01-01'
         self._max_date = f'{self.year}-12-31'
         self._tier = tier
         self._classification = classification
 
-        _tier_search = '&'.join([f'Tier[]={t}' for t in self._tier])
-        _classification_search = '&'.join([f'Classification[]={c}' for c in self._classification])
+        self._search_params = {
+            'event': self._event_name,
+            'date_filter_min': self._min_date ,
+            'date_filter_max': self._max_date,
+            'tier': self._tier,
+            'classification': self._classification
+        }
 
-        self.search_url = self._base_url \
-                            + 'OfficialName=' + self._event_name \
-                            + '&date_filter[min][date]=' + self._min_date \
-                            + '&date_filter[max][date]=' + self._max_date \
-                            + '&' + _tier_search \
-                            + '&' + _classification_search
+        EventSearch.__init__(self, search_type='Event', **self._search_params)
 
-        print(f'Searching here: {self.search_url}')
-        
-        soup = soupify(self.search_url)
+        print(self.event_url)
 
-        event_details = event_parser_initial(self.search_url)
-        
-        self.event_url = event_details[0]
-        self.event_official_name = event_details[1]
-        self.event_number = event_details[2]
+        self.results_df = self.event_parser(self.event_url)
 
-        self.results_df = event_parser_results(self)
 
 
     def __repr__(self):
         return self.event_official_name
+
+
+    def row_parser(self, row):
+        if row.select('td[class*="par"]'):
+            score = row.select('td[class*="par"]')[0].text
+        elif row.select('td[class*="dnf"]'):
+            score = "DNF"
+        else:
+            score = "ERROR"
+        result = { # could also do round scores, round ratings, and total score
+            'Place': int(row.select_one('td[class*="place"]').text),
+            'Player': row.select_one('td[class*="player"]').text,
+            'PDGA Number': int(row.select_one('td[class*="pdga-number"]').text),
+            'Player Rating': int(row.select_one('td[class*="player-rating"]').text),
+            'Score': score,
+        }
+        return result
+
+
+    def event_parser(self, url):
+        
+        soup = self.soupify(url)
+        soup_table = soup.select('div[class*="leaderboard"]')[0]
+        results_table_raw = soup_table.select('div[class*="table-container"]')[0]
+        odd_rows = results_table_raw.select('tr[class*="odd"]')
+        even_rows = results_table_raw.select('tr[class*="even"]')
+        results_raw = [x for x in itertools.chain.from_iterable(itertools.zip_longest(odd_rows,even_rows)) if x]
+        results_list = [self.row_parser(row) for row in results_raw]
+        results_df = pd.DataFrame(data=results_list) #.set_index('Place')
+            
+        return results_df
 
 
     def save_event_results(self, file_path=''):
